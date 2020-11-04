95e66ce9c5759ff5b06665a8fccac62eb3cf3223
Vlad Kirilenko <v.kirilenko@andersensoft.ru>
2020-11-04 15:40:49 +0200

    Basic project implementation
    Fix babel issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Next js App</title>
       </Head>
-      <h3>Possible Improvements</h3>
+      <h3>Possible improvements</h3>
       <ul>
         <li>Adding state machine</li>
         <li>Moving translation to separates files</li>
